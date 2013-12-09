a66355cfb7f2a3aae825429b3cd96a84639e8c0a
adisood <adisood@yahoo.com>
2013-12-08 22:18:43 -0500

    Added LeDeviceListAdapter

--- src/deepble/navigation/DeviceScanActivity.java
+++ src/deepble/navigation/DeviceScanActivity.java
@@ -1,14 +1,26 @@
 package deepble.navigation;
 
-import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+ 
+import java.util.ArrayList;
 
 public class DeviceScanActivity extends Activity {
 	// private LeDeviceListAdapter mLeDeviceListAdapter;
@@ -16,6 +28,7 @@ public class DeviceScanActivity extends Activity {
 	@SuppressWarnings("unused")
     private boolean mScanning;
     private Handler mHandler;
+    private LeDeviceListAdapter mLeDeviceListAdapter;
 	
     private static final int REQUEST_ENABLE_BT = 1;
     private static final long SCAN_PERIOD = 10000;
@@ -81,12 +94,60 @@ public class DeviceScanActivity extends Activity {
 	           
 	           public void run() {
 	        	   //TODO - Look into LeDeviceListAdapter, what it does, why we need it
-	        	   
-	               //mLeDeviceListAdapter.addDevice(device);
+	        	   mLeDeviceListAdapter = new LeDeviceListAdapter();
+	               mLeDeviceListAdapter.addDevice(device);
 	               //mLeDeviceListAdapter.notifyDataSetChanged();
 	           }
 	       });
 	   }
 	};
+	
+	// Adapter for holding devices found through scanning.
+	private class LeDeviceListAdapter extends BaseAdapter {
+	private ArrayList<BluetoothDevice> mLeDevices;
+	        private LayoutInflater mInflator;
+	
+	        public LeDeviceListAdapter() {
+	            super();
+	            mLeDevices = new ArrayList<BluetoothDevice>();
+	            mInflator = DeviceScanActivity.this.getLayoutInflater();
+	        }
+	
+	        public void addDevice(BluetoothDevice device) {
+	            if(!mLeDevices.contains(device)) {
+	                mLeDevices.add(device);
+	            }
+	        }
+	
+	        public BluetoothDevice getDevice(int position) {
+	            return mLeDevices.get(position);
+	        }
+	
+	        public void clear() {
+	            mLeDevices.clear();
+	        }
+	
+	        @Override
+	        public int getCount() {
+	            return mLeDevices.size();
+	        }
+	
+	        @Override
+	        public Object getItem(int i) {
+	            return mLeDevices.get(i);
+	        }
+	
+	        @Override
+	        public long getItemId(int i) {
+	            return i;
+	        }
+
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				// TODO Possibly get back to this later.
+				return null;
+			}
+	      
+	    }
 
 }
